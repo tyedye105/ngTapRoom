6e2e3230be09bf4a867c112c65f450cafa8581e6
Brad Copenhaver and Marty Kovach <brad@>
2017-01-18 10:58:17 -0800

    add abv warning color for high abv kegs

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,22 +7,27 @@ import { Component } from '@angular/core';
   <h1> Weclome To the Tap Room! </h1>
     <h3>Currently on Tap:</h3>
     <ul>
-      <li [class]="kegChange(currentKeg)" *ngFor="let currentKeg of kegs">{{currentKeg.name}}, by {{currentKeg.brewer}}, \${{currentKeg.price}} per pint, {{currentKeg.abv}}% ABV. {{currentKeg.pints}} pints remaining<button (click)="editKeg(currentKeg)">Edit Keg</button><button (click)="sellPint(currentKeg)">Sell pint!</button></li>
+      <li *ngFor="let currentKeg of kegs">{{currentKeg.name}}, by {{currentKeg.brewer}}, \${{currentKeg.price}} per pint, <span [class]="abvWarning(currentKeg)">{{currentKeg.abv}}% ABV</span>. {{currentKeg.pints}} pints remaining</li>
+    </ul>
+    <hr>
+    <h3>Employee Portal:</h3>
+    <ul>
+      <li [class]="kegChange(currentKeg)" *ngFor="let currentKeg of kegs">{{currentKeg.name}}, by {{currentKeg.brewer}}, \${{currentKeg.price}} per pint, {{currentKeg.abv}}% ABV. {{currentKeg.pints}} pints remaining <button (click)="editKeg(currentKeg)">Edit Keg</button> <button (click)="sellPint(currentKeg)">Sell pint!</button></li>
     </ul>
     <div  *ngIf="selectedKeg">
-    <h3>Edit {{selectedKeg.name}} by {{selectedKeg.brewer}}</h3>
-    <label>Beer name:</label>
-    <input [(ngModel)]="selectedKeg.name">
-    <label>Brewer:</label>
-    <input [(ngModel)]="selectedKeg.brewer">
-    <label>Price:</label>
-    <input [(ngModel)]="selectedKeg.price">
-    <label>ABV:</label>
-    <input [(ngModel)]="selectedKeg.abv">
-    <label>Pints Left:</label>
-    <input [(ngModel)]="selectedKeg.pints">
-    <button (click)="finishedEditing()">Done</button>
-  </div>
+      <h3>Edit {{selectedKeg.name}} by {{selectedKeg.brewer}}</h3>
+      <label>Beer name:</label>
+      <input [(ngModel)]="selectedKeg.name">
+      <label>Brewer:</label>
+      <input [(ngModel)]="selectedKeg.brewer">
+      <label>Price:</label>
+      <input [(ngModel)]="selectedKeg.price">
+      <label>ABV:</label>
+      <input [(ngModel)]="selectedKeg.abv">
+      <label>Pints Left:</label>
+      <input [(ngModel)]="selectedKeg.pints">
+      <button (click)="finishedEditing()">Done</button>
+    </div>
   </div>
 `
 })
@@ -31,12 +36,21 @@ export class AppComponent {
   kegs: Keg[] = [
     new Keg("IPA", "Happy Brews", 5.00, 5.4),
     new Keg("Stout", "Unhappy Brews", 5.00, 6.4),
-    new Keg("Blueberry Cider", "Blue's Brews", 6.00, 4.2)
+    new Keg("Blueberry Cider", "Blue's Brews", 6.00, 4.2),
+    new Keg("Triple Imperial Barrel-aged Barley Wine", "Crazy Joe", 13, 19.7)
   ]
   selectedKeg = null;
 
   kegChange(currentKeg) {
     if (currentKeg.pints <= 10) {
+      return "bg-danger";
+    } else {
+      return "" ;
+    }
+  }
+
+  abvWarning(currentKeg) {
+    if (currentKeg.abv >= 7) {
       return "bg-danger";
     } else {
       return "" ;
