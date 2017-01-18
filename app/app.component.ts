a7146ed14df7b1154c63358e6d7e129660d77524
Brad Copenhaver and Marty Kovach <marty@>
2017-01-18 09:42:47 -0800

    initial commit, create angular app structure.

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+  <h1> Weclome To the Tap Room! </h1>
+  </div>
+`
+})
+
+export class AppComponent { }
