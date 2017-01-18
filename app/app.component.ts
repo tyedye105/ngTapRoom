65cde4ac400c5c8b7be0c6004261ed6afbf61e98
Brad Copenhaver and Marty Kovach <brad@>
2017-01-18 09:57:46 -0800

    create keg object, display on index

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,8 +5,16 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
   <h1> Weclome To the Tap Room! </h1>
+    <h3>{{firstKeg.name}}, {{firstKeg.brewer}}</h3>
   </div>
 `
 })
 
-export class AppComponent { }
+export class AppComponent {
+  firstKeg: Keg = new Keg("IPA", "Happy Brews", 5.00, 5.4);
+ }
+
+export class Keg {
+  public pints: number = 124;
+  constructor(public name: string,public brewer: string,public price: number,public abv: number) { }
+}
